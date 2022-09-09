62cc9851068b617d654be60396ec9d627ab63678
Behlül Bozal <57594143+behlulbozal@users.noreply.github.com>
2022-09-09 15:59:22 +0300

    change project

---
+++ src/LocationSearchServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+    namespace behlulbozal\LocationSearch;
+
+    use Illuminate\Support\ServiceProvider;
+    class LocationSearchServiceProvider extends ServiceProvider {
+        public function boot()
+        {
+        }
+        public function register()
+        {
+      }
+   }
